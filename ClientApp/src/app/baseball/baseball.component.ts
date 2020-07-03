75248287a37b5a8290a766a104af30309ea41ede
Michael Blum <Michael.Blum@prosearch.us>
2020-07-03 01:02:00 -0700

    baseball data can be requested from ui dropdowns.

--- ClientApp/src/app/baseball/baseball.component.ts
+++ ClientApp/src/app/baseball/baseball.component.ts
@@ -21,6 +21,30 @@ export class BaseballComponent {
 
     ngOnInit() {
         this.baseballHTTPService.getPlayer()
-        .subscribe((data: any) => { console.log(data)});
+        .subscribe((data: any) => { console.log(data); });
+    }
+
+    handleDropdownClick(teamCode: string) {
+        this.baseballHTTPService.get40ManRoster(teamCode)
+        .subscribe((data: any[]) => {
+
+            console.log(data);
+
+            const returnedData = data['roster_40']['queryResults']['row'];
+            const stuff = [];
+
+            returnedData.forEach(player => {
+                const playerName = player['name_display_first_last'];
+                const position = player['position_txt'];
+                const weight = player['weight'];
+                const playa = [playerName, position, weight,  0.300, 45, 100, 1.000, 130, 30, 3];
+                stuff.push(playa);
+            });
+            this.myData = stuff;
+        });
+    }
+
+    onSelect(event: any){
+        console.log(event);
     }
 }
--- ClientApp/src/app/shared/services/baseballHTTP.service.ts
+++ ClientApp/src/app/shared/services/baseballHTTP.service.ts
@@ -7,11 +7,50 @@ import { catchError, retry } from 'rxjs/operators';
 export class BaseballHTTPService {
 
     private readonly SERVICE_URL = 'https://lookup-service-prod.mlb.com';
+    private readonly GET_40_MAN_ROSTER = `/json/named.roster_40.bam?team_id=`;
+
+    teamCodeMap: Map<string, string> = new Map([
+        ['bal', '110'],
+        ['box', '111'],
+        ['nya', '147'],
+        ['tba', '139'],
+        ['tor', '141'],
+        ['cha', '145'],
+        ['cle', '114'],
+        ['det', '116'],
+        ['kca', '118'],
+        ['min', '142'],
+        ['hou', '117'],
+        ['ana', '108'],
+        ['oak', '133'],
+        ['sea', '136'],
+        ['tex', '140'],
+        ['atl', '144'],
+        ['mia', '146'],
+        ['nym', '121'],
+        ['phi', '143'],
+        ['chn', '112'],
+        ['cin', '113'],
+        ['mil', '158'],
+        ['pit', '134'],
+        ['sln', '138'],
+        ['ari', '109'],
+        ['col', '115'],
+        ['lan', '119'],
+        ['sdn', '135'],
+        ['sfn' , '137']
+    ]);
 
     constructor(private http: HttpClient) { }
 
     getPlayer() {
-        const uri = `${this.SERVICE_URL}/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='cespedes%25'`;
+        const uri = `${this.SERVICE_URL}/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='cueto%25'`;
+        console.log(uri);
+        return this.http.get(uri);
+    }
+
+    get40ManRoster(teamCode: string) {
+        const uri = `${this.SERVICE_URL}${this.GET_40_MAN_ROSTER}${teamCode}`;
         console.log(uri);
         return this.http.get(uri);
     }
