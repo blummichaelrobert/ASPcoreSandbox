09fe86a267be52fceca438e73c5da2ec2d6ff0e2
Michael Blum <Michael.Blum@prosearch.us>
2021-03-12 15:09:10 -0800

    change global font size and add components for baseball.

---
+++ ClientApp/src/baseball/baseball-container.js
@@ -0,0 +1,23 @@
+﻿import React, { Component } from "react";
+import { SelectedTeam } from "./selected-team";
+
+export class BaseballContainer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	getYankees() {
+		alert('Fetching Yankees!');
+	}
+
+	render() {
+		return (
+			<div style={{backgroundColor: 'grey'}}>
+				<h1>Baseball Works!</h1>
+				<button onClick={() => this.getYankees()}>Get Yankees</button>
+				<SelectedTeam></SelectedTeam>
+			</div>
+		);
+	}
+}
---
+++ ClientApp/src/baseball/selected-team.js
@@ -0,0 +1,26 @@
+﻿import React, { Component } from "react";
+
+
+export class SelectedTeam extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div style={{width: '30vw'}}>
+                <h3>Selected Team</h3>
+                <div>P:</div>
+                <div>C:</div>
+                <div>1B:</div>
+                <div>2B:</div>
+                <div>3B:</div>
+                <div>SS:</div>
+                <div>OF:</div>
+                <div>OF:</div>
+                <div>OF:</div>
+                <div>DH:</div>
+            </div>
+        );
+    }
+}
